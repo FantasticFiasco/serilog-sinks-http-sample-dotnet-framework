f593799176ec2916079f8d4ce3970278615dfaeb
FantasticFiasco <mattias.kindborg@gmail.com>
2018-04-30 07:28:31 +0200

    feat: add log event generator

---
+++ serilog/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using Serilog.Generators;
+
+namespace Serilog
+{
+    class Program
+    {
+        static void Main()
+        {
+            ILogger logger = new LoggerConfiguration()
+                .WriteTo.DurableHttp("http://logstash:8080")
+                .WriteTo.Console()
+                .CreateLogger()
+                .ForContext<Program>();
+
+            var customerGenerator = new CustomerGenerator();
+            var orderGenerator = new OrderGenerator();
+
+            while (true)
+            {
+                var customer = customerGenerator.Generate();
+                var order = orderGenerator.Generate();
+
+                logger.Information("{@customer} placed {@order}", customer, order);
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
