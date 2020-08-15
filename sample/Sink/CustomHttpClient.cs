df8b1cc32ba91b3d36e0fb659e0102aa3f4161fc
FantasticFiasco <mattias.kindborg@gmail.com>
2020-08-15 22:58:37 +0200

    chore: update dependencies

--- sample/Sink/CustomHttpClient.cs
+++ sample/Sink/CustomHttpClient.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Serilog.Sinks.Http;
 
 namespace Sample.Sink
@@ -8,11 +9,9 @@ namespace Sample.Sink
     {
         private readonly HttpClient httpClient;
 
-        public CustomHttpClient()
-        {
-            httpClient = new HttpClient();
-            httpClient.DefaultRequestHeaders.Add("X-Api-Key", "secret-api-key");
-        }
+        public CustomHttpClient() => httpClient = new HttpClient();
+
+        public void Configure(IConfiguration configuration) => httpClient.DefaultRequestHeaders.Add("X-Api-Key", "secret-api-key");
 
         public Task<HttpResponseMessage> PostAsync(string requestUri, HttpContent content) => httpClient.PostAsync(requestUri, content);
 
