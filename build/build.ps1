29a5cac3fbe94065472fdca466a290db3b10d3cf
FantasticFiasco <mattias@kindb.org>
2024-03-29 15:27:04 +0100

    wip

--- build/build.ps1
+++
@@ -1,7 +0,0 @@
-$logo = (Invoke-WebRequest "https://raw.githubusercontent.com/FantasticFiasco/logo/master/logo.raw").toString();
-Write-Host "$logo" -ForegroundColor Green
-
-& nuget restore
-& msbuild /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
-
-if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode)  }
